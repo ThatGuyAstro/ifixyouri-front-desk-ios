2ff82270f36a4689d219d48dcbc426b1b017beae
Sam Elder <simplicityapplications@gmail.com>
2017-11-09 17:07:28 -0500

    init commit

---
+++ LandingCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  LandingCollectionViewCell.swift
+//  iFixYouri Front Desk
+//
+//  Created by Sam Elder on 11/9/17.
+//  Copyright © 2017 Sam Elder. All rights reserved.
+//
+
+import UIKit
+
+class LandingCollectionViewCell: UICollectionViewCell {
+    
+}
